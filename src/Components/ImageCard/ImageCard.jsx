bf165352a2ea1c03a3c22b3f5202c6eceff60308
vipinrao009 <vipinrao2018@gmail.com>
2024-03-22 14:46:21 +0530

    Adding css

--- src/Components/ImageCard/ImageCard.jsx
+++ src/Components/ImageCard/ImageCard.jsx
@@ -1,9 +1,12 @@
  import "./ImageCard.css"
+ import { Link } from "react-router-dom"
  function ImageCard({image}){
     return(
+        <Link to={"/"}>
         <div className="image-card">
             <img className="image" src={image}/>
         </div>
+        </Link>
     )
  }
 
